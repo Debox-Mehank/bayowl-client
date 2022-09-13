859aae90a40220c58f1243aa82617ed5bac3937e
tech-inradius <tech@inradius.in>
2022-09-13 19:35:23 +0530

    stitched upload page

--- components/reusable/Button.tsx
+++ components/reusable/Button.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 type ButtonProps = {
   children: JSX.Element;
-  onClick?: () => void;
-  disabled?: boolean
+  onClick?: (e?: any) => void;
+  disabled?: boolean;
 };
 
 function Button({ children, onClick, disabled }: ButtonProps) {
@@ -12,7 +12,11 @@ function Button({ children, onClick, disabled }: ButtonProps) {
       {/* <div className="p-4 z-40 absolute inset-0.5 bg-gradient-to-br from-pink-600 to-blueGradient-3 rounded-lg blur opacity-70 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div> */}
       <button
         onClick={onClick}
-        className={`${disabled ? "bg-white/10 text-white/40 cursor-not-allowed" : "border-gradient-btn"} gradient-border-2 rounded-lg px-4 py-2 font-bold items-center justify-center transition-colors duration-300 z-[60] text-sm md:text-base w-full`}
+        className={`${
+          disabled
+            ? "bg-white/10 text-white/40 cursor-not-allowed"
+            : "border-gradient-btn"
+        } gradient-border-2 rounded-lg px-4 py-2 font-bold items-center justify-center transition-colors duration-300 z-[60] text-sm md:text-base w-full`}
       >
         {children}
       </button>

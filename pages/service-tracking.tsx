763925a7cc73c171cb9c0cdd19bd0999becf91fa
vyas-meet <meetvyas.dev@gmail.com>
2022-09-18 10:55:37 +0530

    add revision modal

--- pages/service-tracking.tsx
+++ pages/service-tracking.tsx
@@ -12,6 +12,11 @@ import { getStatusNames } from "../components/reusable/Accordion";
 import Link from "next/link";
 import moment from "moment";
 import Modal from "../components/reusable/Modal";
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+
+
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -44,6 +49,7 @@ function ServiceTracking() {
   const [tableHeaders, setTableHeaders] = useState<string[]>(TABLE_HEADERS);
   const [isRevModalOpen, setIsRevModalOpen] = useState<boolean>(false)
   const [revNotes, setRevNotes] = useState<string>('')
+  const [revFor, setRevFor] = useState<string>("Original Delivery")
 
   useEffect(() => {
     if (data?.me) {
@@ -262,8 +268,27 @@ function ServiceTracking() {
                                   />
                                 </svg>
 
+                                {
+                                  transaction.revisionFiles.length > 0 && <>
+                                    <p className="mb-4">Which version are you requesting the revision for?</p>
+                                    <div className="relative inline-flex mb-4">
+                                      <svg className="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero" /></svg>
+                                      <select
+                                        value={revFor}
+                                        onChange={(e) => setRevFor(e.target.value)}
+                                        className="border bg-white/5 border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
+                                        <option>Original Delivery</option>
+                                        {transaction.revisionFiles.map((version, index) => (
+                                          <option key={version.revision}>Revision {index + 1}</option>
+                                        ))}
+                                      </select>
+                                    </div>
+                                  </>
+                                }
+
+
                                 <textarea
-                                  className="bg-darkBlue/20 rounded-xl w-full h-[60%] my-2 placeholder:text-center"
+                                  className="bg-darkBlue/20  rounded-xl w-full h-[60%] my-2 placeholder:text-center"
                                   name="Remarks"
                                   id="remarks"
                                   placeholder="Please enter notes for the engineer here."

b0c12f4196dcb7da78c3d4570506be271fcaa174
tech-inradius <tech@inradius.in>
2022-09-27 11:58:47 +0530

    ui changes

--- components/reusable/Button.tsx
+++ components/reusable/Button.tsx
@@ -6,27 +6,22 @@ type ButtonProps = {
   disabled?: boolean;
 };
 
-const Button = React.forwardRef(
-  ({ children, onClick, disabled }: ButtonProps, ref) => {
-    return (
-      <div
-        className={`relative inline-block group text-center w-full`}
-        // ref={() => ref}
+const Button = ({ children, onClick, disabled }: ButtonProps) => {
+  return (
+    <div className={`relative inline-block group text-center w-full`}>
+      <button
+        type="button"
+        onClick={disabled ? () => {} : onClick}
+        className={`${
+          disabled
+            ? "bg-white/10 text-white/40 cursor-not-allowed"
+            : "border-gradient-btn"
+        } gradient-border-2 rounded-lg px-4 py-2 font-bold items-center justify-center transition-colors duration-300 z-[60] text-sm md:text-base w-full`}
       >
-        <button
-          type="button"
-          onClick={disabled ? () => {} : onClick}
-          className={`${
-            disabled
-              ? "bg-white/10 text-white/40 cursor-not-allowed"
-              : "border-gradient-btn"
-          } gradient-border-2 rounded-lg px-4 py-2 font-bold items-center justify-center transition-colors duration-300 z-[60] text-sm md:text-base w-full`}
-        >
-          {children}
-        </button>
-      </div>
-    );
-  }
-);
+        {children}
+      </button>
+    </div>
+  );
+};
 
 export default Button;
